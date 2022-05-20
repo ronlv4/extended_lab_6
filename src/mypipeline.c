13477dc68e254e9c22bd0b9d5c5825af28a49064
ronlv4 <ronlv4@gmail.com>
2022-05-20 17:41:59 +0300

    following oreders task 0b

--- src/mypipeline.c
+++ src/mypipeline.c
@@ -3,11 +3,13 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    int pipe_fd[2];
+    int pipe_fd[2]; /* {read_end, write_end} */
     pipe(pipe_fd);
+    int err_fd = STDERR_FILENO;
     int duplicated_read, duplicated_write;
     int wstatus1, wstatus2;
     pid_t ch1_pid;
+    dprintf(err_fd, "(parent_process>forking...)");
     ch1_pid = fork();
     if (ch1_pid == -1)
     {
@@ -15,23 +17,32 @@ int main(int argc, char **argv) {
         _exit(errno);
     }
     if (ch1_pid){
+        dprintf(err_fd, "(parent_process>created process with id: %d)\n", ch1_pid);
+        dprintf(err_fd, "(parent_process>closing the write end of the pipe...)");
         close(pipe_fd[1]);
         pid_t ch2_pid = fork();
         if (ch2_pid){
-            close(STDIN_FILENO);
-            duplicated_write = dup(pipe_fd[0]);
+            dprintf(err_fd, "(parent_process>closing the read end of the pipe...)");
             close(pipe_fd[0]);
-            execvp("tail", {"tail", "-n", "2"});
-        } else{
-            close(pipe_fd[0]);
+            dprintf(err_fd, "(parent_process>waiting for the child processes to terminate...)");
             waitpid(ch1_pid, &wstatus1);
             waitpid(ch2_pid, &wstatus2);
+            dprintf(err_fd, "(parent_process>exiting...)");
+        } else{
+            dprintf(err_fd, "(child2>redirecting stdin to the read end of the pipe...)");
+            close(STDIN_FILENO);
+            duplicated_read = dup(pipe_fd[0]);
+            close(pipe_fd[0]);
+            dprintf(err_fd, "(child2>going to execute cmd: ...)");
+            execvp("tail", {"tail", "-n", "2"});
         }
 
     } else{
+        dprintf(err_fd, "(child1>redirecting stdout to the write end of the pipe...)");
         close(STDOUT_FILENO);
-        duplicated_read = dup(pipe_fd[1]);
+        duplicated_write = dup(pipe_fd[1]);
         close(pipe_fd[1])
+        dprintf(err_fd, "(child1>going to execute cmd: ...)");
         execevp("ls", {"ls", "-l"});
     }
 }
